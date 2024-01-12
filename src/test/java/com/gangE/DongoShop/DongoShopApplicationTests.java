0d05ce96a516eecfa75b2f92dd21955a89a71a56
sengwoong <rje287573@gmail.com>
2024-01-12 12:14:03 +0900

    :sparkles: Feat: 프로젝트 세팅

---
+++ src/test/java/com/gangE/DongoShop/DongoShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gangE.DongoShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DongoShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
